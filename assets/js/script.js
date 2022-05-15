8dd27e5b70f5ce365d97a7bf82ae350b3766bc69
Rich Arce <raarce118@gmail.com>
2022-05-14 19:00:54 -0700

    created acc and added API

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,15 +21,15 @@ var citySubmitHandler = function(event) {
 
 var getForecast = function(city) {
     // * Need request to the url here
-    fetch("")
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=a41bbe96ed348f5aea3213a8cbc2f640")
     .then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
 
                 var {lat} = data.coord;
                 var {lon} = data.coord;
-
-    fetch("")
+     // * need lat and lon API
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=a41bbe96ed348f5aea3213a8cbc2f640`)
         .then = function(response) {
             if(response.ok) {
                 response.json().then(function(data) {
@@ -47,4 +47,18 @@ var getForecast = function(city) {
     alert("Unable to connect");
 });
 
+function displayForecast(fc, name, humidity, wind) {
+    var fahrenheit = Math.tound(((parselFloat(fc.main.temp)-273.15)*1.8)+32);
+    var wind = Math.round(fc.wind.speed);
+    
+    document.getElementById('subtitle').innerHTML = name;
+    document.getElementById('subtitle').innerHTML = fahrenheit + '&deg;';
+    document.getElementById('subtitle').innerHTML = wind + 'mph';
+    document.getElementById('subtitle').innerHTML = humidity + '%';
+    
+
 }
+
+}
+
+cityFormEl.addEventListener("submit", citySubmitHandler);
